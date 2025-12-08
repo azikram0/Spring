a10a9a5bd9daee7bba2dd21d0d086c6a24266b7c
Elizaveta <elisavetayapryntseva@yandex.ru>
2025-12-08 21:48:41 +0400

    Added html forms and Application

---
+++ src/main/java/com/azikram0/spring/lab5/Application.java
@@ -0,0 +1,11 @@
+package com.azikram0.spring.lab5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
